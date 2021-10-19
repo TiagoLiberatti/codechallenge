38c7eb6fc38ff9a14401d951bf6384ad6ac61a83
Tiago Liberatti <tiagoliberatti@gmail.com>
2021-10-19 20:29:26 -0300

    feat: criação da aplicação spring

---
+++ codechallenge-api/src/main/java/com/tiagodev/codechallenge/CodechallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiagodev.codechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodechallengeApiApplication.class, args);
+	}
+
+}
---
+++ codechallenge-api/src/test/java/com/tiagodev/codechallenge/CodechallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagodev.codechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
