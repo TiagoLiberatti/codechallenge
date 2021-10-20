f20d5e7e6faf80b56aa49449bbe643df9bb42dfa
Tiago Liberatti <tiagoliberatti@gmail.com>
2021-10-20 12:54:08 -0300

    feat: criando fluxo para quando iniciar a aplicação converter informações de um arquivo planiha para objeto java e realizar a persistência no banco de dados

--- codechallenge-api/src/main/java/com/tiagodev/codechallenge/CodechallengeApiApplication.java
+++ codechallenge-api/src/main/java/com/tiagodev/codechallenge/CodechallengeApiApplication.java
@@ -1,13 +1,26 @@
 package com.tiagodev.codechallenge;
 
+import com.tiagodev.codechallenge.core.usecase.SalvarContasBancariasUseCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodechallengeApiApplication {
+public class CodechallengeApiApplication implements ApplicationRunner {
+
+	@Autowired
+	private SalvarContasBancariasUseCase salvarContaBancariaUseCase;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodechallengeApiApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		this.salvarContaBancariaUseCase.executar();
 	}
 
 }
--- codechallenge-api/src/main/java/com/tiagodev/codechallenge/core/usecase/SalvarContasBancariasUseCase.java
+++ codechallenge-api/src/main/java/com/tiagodev/codechallenge/core/usecase/SalvarContasBancariasUseCase.java
@@ -5,9 +5,19 @@ import com.tiagodev.codechallenge.core.exceptions.UseCaseException;
 import com.tiagodev.codechallenge.core.usecase.gateway.ContaBancariaGateway;
 import com.tiagodev.codechallenge.dataprovider.exceptions.DataProviderException;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -16,7 +26,11 @@ public class SalvarContasBancariasUseCase {
     @Autowired
     private ContaBancariaGateway contaBancariaGateway;
 
-    public List<ContaBancariaEntity> executar(List<ContaBancariaEntity> contaBancariaEntityList){
+    @Value(value = "${diretorio.arquivo.excel}")
+    private String diretorioArquivoExcel;
+
+    public List<ContaBancariaEntity> executar() throws IOException {
+        List<ContaBancariaEntity> contaBancariaEntityList = this.converterExcel();
         return this.salvarContaBancariaUseCase(contaBancariaEntityList);
     }
 
@@ -26,6 +40,30 @@ public class SalvarContasBancariasUseCase {
         }catch (DataProviderException e){
             throw new UseCaseException("Ocorreu um erro ao salvar lista de contas bancarias");
         }
+    }
+
+    private List<ContaBancariaEntity> converterExcel() throws IOException {
+        FileInputStream file = new FileInputStream(diretorioArquivoExcel);
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet worksheet = workbook.getSheetAt(0);
+
+        List<ContaBancariaEntity> contaBancariaTableList = new ArrayList<>();
+        DataFormatter formatter = new DataFormatter();
+
+        for(int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
+            XSSFRow row = worksheet.getRow(i);
+            contaBancariaTableList.add(ContaBancariaEntity.builder()
+                    .nome(row.getCell(0).getStringCellValue())
+                    .numeroConta(formatter.formatCellValue(row.getCell(1)))
+                    .agencia(formatter.formatCellValue(row.getCell(2)))
+                    .chequeEspecialLiberado(row.getCell(3).getNumericCellValue() == 1)
+                    .saldo(new BigDecimal(formatter.formatCellValue(row.getCell(4)).replace(".", "").replace(",", ".")))
+                    .chequeEspecial(new BigDecimal(formatter.formatCellValue(row.getCell(5)).replace(".", "").replace(",", ".")))
+                    .taxa(Double.parseDouble(row.getCell(6).getStringCellValue().replace(".", "").replace(",", ".")))
+                    .build());
+        }
+
+        return contaBancariaTableList;
     }
 
 }
