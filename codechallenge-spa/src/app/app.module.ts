8afeb0e05662913c5c355e56295209d893500529
Tiago Liberatti <tiagoliberatti@gmail.com>
2021-10-20 20:50:28 -0300

    refactor: refatorando componente angular app

--- codechallenge-spa/src/app/app.module.ts
+++ codechallenge-spa/src/app/app.module.ts
@@ -1,14 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContaBancariaCreateComponent } from './conta-bancaria/components/conta-bancaria-create/conta-bancaria-create.component';
+import { ContaBancariaDetailComponent } from './conta-bancaria/components/conta-bancaria-detail/conta-bancaria-detail.component';
+import { ContaBancariaEditComponent } from './conta-bancaria/components/conta-bancaria-edit/conta-bancaria-edit.component';
+import { ContaBancariaFormComponent } from './conta-bancaria/components/conta-bancaria-form/conta-bancaria-form.component';
+import { ContaBancariaListComponent } from './conta-bancaria/components/conta-bancaria-list/conta-bancaria-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContaBancariaListComponent,
+    ContaBancariaCreateComponent,
+    ContaBancariaEditComponent,
+    ContaBancariaDetailComponent,
+    ContaBancariaFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastrModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
