ac2c1a4854939a7d3f4533500dd2a1e5d2ad7e5b
Tiago Liberatti <tiagoliberatti@gmail.com>
2021-10-20 13:04:40 -0300

    feat: criando componente angular para listagem de contas bancárias

---
+++ codechallenge-spa/src/app/conta-bancaria/components/conta-bancaria-list/conta-bancaria-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
+import { ContaBancariaFilterForm } from '../../model/conta-bancaria-filter-form.model';
+import { ContaBancaria } from '../../model/conta-bancaria.model';
+import { ContaBancariaService } from '../../service/conta-bancaria.service';
+
+@Component({
+  selector: 'app-conta-bancaria-list',
+  templateUrl: './conta-bancaria-list.component.html',
+  styleUrls: ['./conta-bancaria-list.component.css']
+})
+export class ContaBancariaListComponent implements OnInit {
+
+  public contasBancarias: ContaBancaria[];
+
+  constructor(
+    private contaBancariaService: ContaBancariaService,
+    private toastr: ToastrService
+  ) {
+    this.contasBancarias = [];
+  }
+
+  ngOnInit(): void {
+    this.atualizarTabela();
+  }
+
+  public atualizarTabela(): void{
+    this.contaBancariaService.buscarContasBancarias().subscribe(
+      data => {
+        if(data){
+          this.contasBancarias = data;
+        }else{
+          this.toastr.error('Nenhuma conta bancária encontrada', '');
+        }
+      },
+      () => {
+        this.toastr.error('Ocorreu um erro ao realizar consulta de contas bancárias', '');
+      },
+    );
+  }
+
+  public deletarContaBancaria(id: number): void{
+    this.contaBancariaService.deletarContaBancaria(id).subscribe(data => {
+      this.toastr.success('Conta bancária excluída com sucesso!', '');
+      this.atualizarTabela();
+    });
+  }
+
+  public filtrar(form: ContaBancariaFilterForm): void{
+    this.contaBancariaService.buscarContasBancarias(form).subscribe(
+      data => {
+        if(data){
+          this.contasBancarias = data;
+        }else{
+          this.toastr.error('Nenhuma conta bancária encontrada', '');
+        }
+      },
+      () => {
+        this.toastr.error('Ocorreu um erro ao realizar consulta de contas bancárias', '');
+      },
+    );
+  }
+
+}
